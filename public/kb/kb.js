fc212bc333f100ac68660ce10330504d954240ac
void <void@gmail.com>
2022-09-10 13:54:34 +0300

    add docs chapters navigation.

--- public/kb/kb.js
+++ public/kb/kb.js
@@ -9,9 +9,8 @@ const { params } = urlParse(window.location.search);
 // appendScripts(['applyWebGlBg', 'TimeReportFieldController', 'standupAdapter']);
 const converter = new showdown.Converter();
 const functions = {
-  rich(markup) {
+  rich(rootElement,markup) {
     const html = converter.makeHtml(markup);
-    const rootElement = this.$impl.$element;
     rootElement.innerHTML = '';
     arrmatura(`${html}`, { rootElement })
   }
@@ -21,46 +20,46 @@ const kbAdapter = (list) => {
   let chapterCursor = null;
   let sectionCursor = null;
   let blockCursor = null;
-  const tree = {};
+  const tree = {
+    main:{ id:'0', name:'', data: {'0':{ id:'0',data:{'0':{id:'0',data:[{id:'hero', type:'hero'}]}}}}}
+  };
 
   list.forEach((item) => {
-    const { $row: id, chapter, section, block, content, ...e } = item;
+    const { $row: id, chapter, section, block, value, ...e } = item;
     if (chapter) {
-      chapterCursor = tree[chapter] = { id,content, name:chapter,  data: {} };
+      chapterCursor = tree[chapter] = { id:chapter,value, name:chapter,  data: {} };
       sectionCursor = null;
       blockCursor = null;
     } else if (section) {
-      sectionCursor = chapterCursor.data[section] = { id:chapterCursor.id+'-'+id, name: section,content, data: {} }
+      sectionCursor = chapterCursor.data[section] = { id:chapterCursor.id+'/'+id, name: section,value, data: {} }
       blockCursor = null;
     } else if (block) {
       if (!sectionCursor) {
-        sectionCursor = chapterCursor.data['-'] = { id:chapterCursor.id+'-0', data: {} }
+        sectionCursor = chapterCursor.data['-'] = { id:chapterCursor.id+'/0', data: {} }
       }
-      blockCursor = sectionCursor.data[block] = { id:chapterCursor.id+'-'+sectionCursor.id+'-'+id, name: block,content, data: [] };
+      blockCursor = sectionCursor.data[block] = { id:chapterCursor.id+'/'+sectionCursor.id+'/'+id, name: block,value, data: [] };
     } else {
       if (!chapterCursor) {
         chapterCursor = tree['0'] = { id:'0', data: {} }
       }
       if (!sectionCursor) {
-        sectionCursor = chapterCursor.data['-'] = { id:chapterCursor.id+'-0', data: {} }
+        sectionCursor = chapterCursor.data['-'] = { id:chapterCursor.id+'/0', data: {} }
       }
       if (!blockCursor) {
-        blockCursor = sectionCursor.data['-'] = { id:chapterCursor.id+'-'+sectionCursor.id+'-0', data: [] }
+        blockCursor = sectionCursor.data['-'] = { id:chapterCursor.id+'/'+sectionCursor.id+'/0', data: [] }
       }
-      blockCursor.data.push({...e,name:e.item, content, id});
+      blockCursor.data.push({...e,name:e.key, value, id});
     }
   });
 
-  const data = Object.values(tree)
-    .map(chapter => ({
-      ...chapter,
+  Object.values(tree).forEach(chapter => Object.assign(chapter, {
       data: Object.values(chapter.data).map(section => ({
         ...section,
         data: Object.values(section.data)
       }))
     }));
 
-  return { data }
+  return { data: tree }
 }
 
 const xmls = ['kb/kb.json', 'kb/kb.xml'];
